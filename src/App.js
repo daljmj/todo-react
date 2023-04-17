3ea8184011d30abf81f7ee661d26efd95688c698
daljmj <126189963+daljmj@users.noreply.github.com>
2023-04-18 00:57:05 +0900

    React 이벤트 핸들러로 목록 추가하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Todo from './Todo';
-import {Paper, List}from "@material-ui/core";
+import AddTodo from './AddTodo';
+import {Paper, List, Container}from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
@@ -8,10 +9,19 @@ class App extends React.Component {
     super(props);
     this.state ={
       items :[
-        {id:0, title:"Todo 1 ", done:true},
-        {id:1, title:"Todo 2 ", done:false},
+        {id:"todo0", title:"Todo 1 ", done:true},
+        {id:"todo1", title:"Todo 2 ", done:false},
       ],
     };
+  }
+  // add 함수 추가
+  add =(item)=>{
+    const thisItems =this.state.items;
+    item.id ="ID-"+ thisItems.length;
+    item.done =false;
+    thisItems.push(item);
+    this.setState({items:thisItems});
+    console.log("items:",this.state.items);
   }
   render(){
     var todoItems =this.state.items.length >0 &&(
@@ -23,8 +33,15 @@ class App extends React.Component {
         </List>
       </Paper>
     );
-
-    return <div className="App">{todoItems}</div>;
+    // add 함수 연결
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add}/>
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 
